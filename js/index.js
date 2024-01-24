dfc27db323c154171d0b42d1167ff61c804be44c
Paul Johann Flade <paul-johann@flade-privat.de>
2024-01-24 13:01:12 +0100

    Challange solved

--- js/index.js
+++ js/index.js
@@ -2,6 +2,7 @@ console.clear();
 
 const form = document.querySelector("form");
 const output = form.querySelector("output");
+const errorElement = document.querySelector('[data-js="errorElement"]');
 
 const operations = {
   add: (a, b) => a + b,
@@ -17,8 +18,15 @@ const operations = {
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
+  errorElement.textContent = "";
   const firstNumber = Number(event.target.firstNumber.value);
   const secondNumber = Number(event.target.secondNumber.value);
   const operation = event.target.operation.value;
-  output.innerText = operations[operation](firstNumber, secondNumber);
+  try {
+    const result = operations[operation](firstNumber, secondNumber);
+    output.innerText = result;
+  } catch (error) {
+    console.log("Dosn't work");
+    errorElement.textContent = "Division throw Zero issn't possible";
+  }
 });
